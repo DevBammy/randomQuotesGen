2605ef088b864db29656489de9f09609cc9a27ec
devBammy <devbammy@gmail.com>
2023-02-03 11:34:57 +0100

    Built the whole app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import React from "react";
-import "./index.scss"
+import "./index.scss";
+import { useState } from "react";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+
+  const getAdvice = () => {
+    fetch("	https://api.adviceslip.com/advice").then((res) => {
+      return res.json().then((data) => {
+        setAdvice(data.slip);
+        console.log(advice);
+      });
+    });
+  };
+
   return (
     <>
-      <h1>My App!!!</h1>
-      <p>Template by devBammy</p>
-      <a href="/">Call Me</a>
-      <p>NOW!</p>
+      <div className="container">
+        <h1 className="advice">{advice.advice}</h1>
+        <button className="btn" onClick={getAdvice}>
+          Get Advice
+        </button>
+      </div>
     </>
   );
 }
